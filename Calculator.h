90e1b4d3963108a5be9a79a712daed9637a822b7
taerimiiii <shipton0201@gmail.com>
2024-11-20 18:59:44 +0900

    Update Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include "LinkedListStack.h"
 
+// 괄호, 연산자, 공백, 피연산자 상수 선언
 typedef enum {
 	LEEF_PARENTHESIS = '(', RIGHT_PARENTHESIS = ')',
 	PLUS = '+', MINUS = '-',
@@ -11,6 +12,7 @@ typedef enum {
 	SPACE = ' ', OPERAND
 } SYMBOL;
 
+// 스택계산기 구현에 활용할 함수 선언
 int IsNumber(char Cipher);
 unsigned int GetNextToken(char* Expression, char* Token, int* TYPE);
 int IsPrior(char Operator1, char Operator2);
